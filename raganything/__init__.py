904b7eaf3263da0edf2775c753d11ef36ff54e9b
YoloBird <yolobird@foxmail.com>
2025-10-24 16:40:46 +0800

    “add_branch”

--- raganything/__init__.py
+++ raganything/__init__.py
@@ -5,4 +5,5 @@ __version__ = "1.2.8"
 __author__ = "Zirui Guo"
 __url__ = "https://github.com/HKUDS/RAG-Anything"
 
+# 两个主要类，RAGAnything与RAGAnythingConfig
 __all__ = ["RAGAnything", "RAGAnythingConfig"]
